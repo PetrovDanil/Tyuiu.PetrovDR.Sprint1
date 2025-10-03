203e42111f33ae97061a92c5084f4eb1260735d9
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-10-03 21:09:41 +0500

    Add project files.

---
+++ Tyuiu.PetrovDR.Sprint1.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PetrovDR.Sprint1.Task0.V29.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint1.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate;
+            Assert.AreEqual(32, res);
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint1.Task0.V29/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PetrovDR.Sprint1.Task0.V29.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint1.Task0.V29
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 75;
+
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #1", width);
+            PrintCenteredLine("Тема: Базовые навыки работы в C#", width);
+            PrintCenteredLine("Задание #0", width);
+            PrintCenteredLine("Вариант #29", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            PrintCenteredLine("Написать программу, которая вычисляет выражение 2*9+7*2", width);
+            PrintCenteredLine("и печатает результат на экране.", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("2*9+7*2", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+            Console.WriteLine(new string('*', width));
+
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+        
+    }
+}
