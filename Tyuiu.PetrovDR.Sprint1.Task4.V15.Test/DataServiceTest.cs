0718b99b6f048f02e0f583ace2a3421254eb4c9c
PetrovDanil <Tyuiu.PetrovDR@outlook.com>
2025-10-04 10:43:32 +0500

    t4

---
+++ Tyuiu.PetrovDR.Sprint1.Task4.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PetrovDR.Sprint1.Task4.V15.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint1.Task4.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = Math.Round(2420.5727, 3);
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint1.Task4.V15/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.PetrovDR.Sprint1.Task4.V15.Lib;
+
+
+namespace Tyuiu.PetrovDR.Sprint1.Task4.V15
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 75;
+
+
+            PrintCenteredLine("Спринт #1 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #1", width);
+            PrintCenteredLine("Тема: Базовые навыки работы в C#", width);
+            PrintCenteredLine("Задание #4", width);
+            PrintCenteredLine("Вариант #15", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            PrintCenteredLine("Написать программу, которая запрашивает у пользователя исходные данные,", width);
+            PrintCenteredLine("вычисляет результат по формуле и печатает его на экране.", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+            Console.WriteLine(new string('*', width));
+
+
+            Console.WriteLine("Введите первое число:", width);
+            double x = Convert.ToDouble (Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:", width);
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+            Console.WriteLine(new string('*', width));
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
